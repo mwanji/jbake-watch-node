cacfd3eeb79024846765e8436e48e4ee5657de18
moandji.ezana <mwanji@gmail.com>
2014-01-23 09:44:49 +0200

    Created binary: jbakew

--- jbake-watch.js
+++ jbake-watch.js
@@ -1,53 +1,50 @@
 var
-  rootDir = process.cwd(),
   spawn = require("child_process").spawn,
   watchr = require("watchr"),
   debounce = require("debounce");
 
-var jbake = (function () {
-    var mode, jbakeProcess;
+module.exports = function (rootDir) {
+  var mode, jbakeProcess;
 
-    var bake = function () {
-      var self = this;
-      if (mode === "serve") {
-        console.log("Stopping JBake server...");
-        jbakeProcess.kill();
+  var bake = function () {
+    var self = this;
+    if (mode === "serve") {
+      console.log("Stopping JBake server.");
+      jbakeProcess.kill();
+    }
+    
+    mode = "bake";
+    jbakeProcess = spawn("jbake");
+    jbakeProcess.stdout.pipe(process.stdout);
+    jbakeProcess.on("close", function (code) {
+      if (code !== 0) {
+        mode = null;
+        console.err("There were errors. JBake server not started.");
+        return;
       }
       
-      mode = "bake";
-      jbakeProcess = spawn("jbake");
-      jbakeProcess.stdout.pipe(process.stdout);
-      jbakeProcess.on("close", function (code) {
-        if (code !== 0) {
-          mode = null;
-          console.err("There were errors. JBake server not started.");
-          return;
-        }
-        
-        self.serve();
-      });
-    };
-    
-    return {
-      bake: debounce(bake, 2000),
-      serve: function () {
-        jbakeProcess = spawn("jbake", ["-s"]);
-        mode = "serve";
-        jbakeProcess.stdout.pipe(process.stdout);
-      }
-    }
-  }());
+      self.serve();
+    });
+  };
   
-watchr.watch({
-  paths: [rootDir],
-  ignorePaths: [rootDir + "/output"],
-  ignoreHiddenFiles: true,
-  listener: function (type, filename) {
-    console.log(type + ": " + filename);
-    jbake.bake();
-  }
-});
-
-jbake.serve();
-
-console.log("Waiting for changes...");
+  return {
+    bake: debounce(bake, 2000),
+    serve: function () {
+      jbakeProcess = spawn("jbake", ["-s"]);
+      mode = "serve";
+      jbakeProcess.stdout.pipe(process.stdout);
+    },
+    watch: function () {
+      var self = this;
+      watchr.watch({
+        paths: [rootDir],
+        ignorePaths: [rootDir + "/output"],
+        ignoreHiddenFiles: true,
+        listener: function (type, filename) {
+          console.log(type + ": " + filename);
+          self.bake();
+        }
+      });
+    }
+  };
+};
